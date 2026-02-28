8d3df6c3904b1f7a7db351ced158769fcddfbe5a
Sonu445Kum <sonuroy1629@gmail.com>
2026-02-28 14:41:39 +0530

    add the content in the useScrollSpy.js file

--- src/components/layouts/Navbar.jsx
+++ src/components/layouts/Navbar.jsx
@@ -1,6 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Code, Menu , X } from 'lucide-react'
+import { NAV_LINKS,PERSONAL_INFO } from '../../utils/constants'
 
 const Navbar = () => {
+    const [isMenuOpen , setIsMenuOpen] = useState(false);
+    const [isScrolled , setIsScrolled] = useState(false);
+    const activeSections = useScrollSpy(NAV_LINKS.map(link =>link.id));
+
+    // useEffect
+    useEffect(()=>{
+        const handleScroll = ()=>{
+            setIsScrolled(window.scrollY > 50);
+        };
+
+        window.addEventListener('scroll',handleScroll);
+        return () =>window.removeEventListener('scroll',handleScroll);
+    },[]);
+
+    const handleNavClick =(sectionId) => {
+        scrollToSection(sectionId);
+        setIsMenuOpen(false);
+    }
   return (
     <div>
       
